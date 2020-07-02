1a57bf9b178c2ea8d736af8ff16c92b7efa1518d
JapmanN <japmannagra20@gmail.com>
2020-07-02 23:16:03 +0000

    Found the ID of the store that is closest to me using my postal code

--- app.js
+++ app.js
@@ -3,3 +3,12 @@
 // ================
 var pizzapi = require("dominos");
 require('dotenv').config();
+
+// ===============================================================
+// FIND ID OF CLOSEST STORE TO ME USING MY POSTAL CODE and SAVE IT
+// ===============================================================
+pizzapi.Util.findNearbyStores(process.env.POSTAL_CODE,'Carryout', function(storeData){
+	var Closest_Store = new pizzapi.Store({
+		ID: storeData.result.Stores[0].StoreID
+	});
+});
